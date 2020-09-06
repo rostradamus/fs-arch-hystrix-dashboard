b0dfc2bad1c0d9805ab1d629a552e3a715007ce3
Ro Lee <rolee0429@gmail.com>
2020-09-07 00:22:29 +0900

    FS-29: Implement Microservice Structure

---
+++ src/main/java/com/fitsight/fsarchhystrixdashboard/FsArchHystrixDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.fitsight.fsarchhystrixdashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FsArchHystrixDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FsArchHystrixDashboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fitsight/fsarchhystrixdashboard/FsArchHystrixDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fitsight.fsarchhystrixdashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FsArchHystrixDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
